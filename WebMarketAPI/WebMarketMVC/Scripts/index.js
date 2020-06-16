3f09cd065542f73f612e817382ffc94400164d3d
Skaipi <mt.skarupski@gmail.com>
2020-06-16 20:20:05 +0200

    Add new default index page

---
+++ WebMarketAPI/WebMarketMVC/Scripts/index.js
@@ -0,0 +1,9 @@
+﻿const HandleCheckbox = (id) => {
+    let target = $(id).children('.checkbox').first();
+
+    if (!target.hasClass('active')) {
+        target.addClass('active');
+    } else {
+        target.removeClass('active');
+    }
+}
