d8ea44999be8722805f89505a44330c9582511a4
Skaipi <mt.skarupski@gmail.com>
2020-06-19 16:51:03 +0200

    javascript bundle test

--- WebMarketAPI/WebMarketMVC/Scripts/index.js
+++ WebMarketAPI/WebMarketMVC/Scripts/index.js
@@ -1,9 +1,17 @@
-﻿const HandleCheckbox = (id) => {
+﻿const Products = $('.product');
+let CategoryFilter = [];
+
+const HandleCheckbox = (id) => {
     let target = $(id).children('.checkbox').first();
+    let category = target.category;
 
     if (!target.hasClass('active')) {
         target.addClass('active');
+        CategoryFilter.push(category);
     } else {
         target.removeClass('active');
+        CategoryFilter.filter((c) => c != category);
     }
+
+    Products.filter((product) => CategoryFilter.includes(product.category));
 }
